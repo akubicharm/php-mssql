eef38b25b0d60ba0855d5e44f75381285b356f0a
Kei Omizo <komizo@redhata.com>
2016-03-02 14:32:31 +0900

    update for mssql

--- index.php
+++ index.php
@@ -1,16 +1,41 @@
 <?php
 
 
-echo "<h1>Hello External DB</h1>";
-$mysqli = new mysqli(getenv("HELLO_PHP_MYSQL_SERVICE_HOST"), getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
-if ($mysqli->connect_error) {
-    exit($mysqli->connect_error);
-} else {
-    $mysqli->set_charset("utf8");
+echo "<h1>Connect MSSql Server on Azure by FreeTDS</h1>"
+
+
+// get environment variables
+$user = 'myadmin'; //getenv("DB_USER");
+$pass = '@Dmin123'; // getenv("DB_PASSWD");
+$host = 'db-komizo.database.windows.net';  //getenv("DB_SERVER_HOST");
+$db = 'sqlserver01';
+$server = '$host' + '\' $db;
+
+// generate connection string
+$connection_string = "DRIVER={SQL Server};SERVER=$server;DATABASE=$database"; 
+$conn = odbc_connect($connection_string,$user,$pass);
+
+if ($conn) {
+    print("Connection established.");
 }
-$result = $mysqli->query("select msg from hello") or exit($mysqli->error());
-echo var_dump($result->fetch_assoc());
-$mysqli->close();
+else {
+    print("Connection could not be established.");
+}
+
+# query the users table for all fields 
+$query = "SELECT count(*) FROM SalesLT.customer;
+
+# perform the query 
+$result = odbc_exec($connect, $query); 
+
+# fetch the data from the database 
+while(odbc_fetch_row($result)) { 
+    $field1 = odbc_result($result, 1); 
+    $field2 = odbc_result($result, 2); 
+    print("$field1 $field2\n"); 
+} 
+
+# close the connection 
+odbc_close($connect); 
 
 ?>
-
